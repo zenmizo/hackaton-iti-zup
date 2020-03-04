ffaa8e20beca37046b5372da6e45223f0162a85d
Zendji <zendji.mizoguchi@gmail.com>
2020-03-04 18:24:09 -0300

    atualização ios

--- hackaton-iOS/hackaton-iOS/Components/ListProductCollection/Cell/ProductCollectionCell.swift
+++ hackaton-iOS/hackaton-iOS/Components/ListProductCollection/Cell/ProductCollectionCell.swift
@@ -25,31 +25,12 @@ class ProductCollectionCell: UICollectionViewCell {
     func setupCell(with product: Product,
                    beagleContext: BeagleContext,
                    renderableDependencies: RenderableDependencies) {
-//        self.contentView.backgroundColor = UIColor(red:0.95, green:0.95, blue:0.95, alpha:1.0)
-//        self.labelName.text = product.name
-//        self.labelDescription.text = product.description
-//        self.image.pathImage(product.imagePath)
-//        self.labelPrice.text = product.name
         
-        self.labelName.text = ""
-        self.labelDescription.text = ""
-        self.labelPrice.text = ""
-        
-        self.yoga.isEnabled = true
-
-        var children: [ServerDrivenComponent] = []
-        children.append(Text(product.name))
-        children.append(Text(product.description))
-//        children.append(NetworkImage(path: product.imagePath))
-
-        let size = Flex.Size(width: UnitValue(value: 100, type: .real), height: UnitValue(value: 100, type: .real))
-
-        let containerCell = Container(children: children, flex: Flex(size: size))
-        let viewCell = containerCell.toView(context: beagleContext, dependencies: renderableDependencies)
-        
-        self.addSubview(viewCell)
-        
-        
+        self.contentView.backgroundColor = UIColor(red:0.95, green:0.95, blue:0.95, alpha:1.0)
+        self.labelName.text = product.name
+        self.labelDescription.text = product.description
+        self.image.pathImage(product.imagePath)
+        self.labelPrice.text = product.name
     }
     
 }
