617709a483987a323c58acf4e39a59819a01e576
Zendji <zendji.mizoguchi@gmail.com>
2020-03-05 14:49:59 -0300

    refactor buider

--- marketplace-bff/src/main/kotlin/itau/iti/marketplace/builder/DetailProductBuilder.kt
+++ marketplace-bff/src/main/kotlin/itau/iti/marketplace/builder/DetailProductBuilder.kt
@@ -1,12 +1,15 @@
 package itau.iti.marketplace.builder
 
+import br.com.zup.beagle.core.Appearance
 import br.com.zup.beagle.core.ServerDrivenComponent
 import br.com.zup.beagle.widget.core.*
 import br.com.zup.beagle.widget.form.Form
 import br.com.zup.beagle.widget.form.FormMethodType
+import br.com.zup.beagle.widget.form.FormSubmit
 import br.com.zup.beagle.widget.layout.Container
 import br.com.zup.beagle.widget.layout.NavigationBar
 import br.com.zup.beagle.widget.layout.Screen
+import br.com.zup.beagle.widget.ui.Button
 import br.com.zup.beagle.widget.ui.NetworkImage
 import br.com.zup.beagle.widget.ui.Text
 import itau.iti.marketplace.components.ListProductComponent
@@ -24,17 +27,39 @@ class DeatailProductBuilder {
 
     fun buildScreen(): Screen {
         var childrenProduct = mutableListOf<ServerDrivenComponent>()
+        childrenProduct.add(getBodyContainer())
+        childrenProduct.add(getSubmitContainer())
 
+        return  Screen(
+                navigationBar = NavigationBar(title =  product.name, showBackButton = true)
+                ,content =
+        Form(path = "add/${product.sku}",
+                method = FormMethodType.POST,
+                child = Container(childrenProduct).applyFlex(Flex(justifyContent = JustifyContent.SPACE_BETWEEN, grow = 1.0)))
+        )
+
+    }
+
+    fun getBodyContainer(): Container {
+        var childrenProduct = mutableListOf<ServerDrivenComponent>()
+        childrenProduct.add(getImageContainer())
+        childrenProduct.add(getInfoContainer())
+        childrenProduct.add(getDescriptionContainer())
+        return Container(childrenProduct)
+    }
+
+    fun getImageContainer(): Container {
         var childrenImage = mutableListOf<ServerDrivenComponent>()
-//        childrenImage.add(NetworkImage(path = this.product.imageUrl))
-        childrenImage.add(NetworkImage(path = "https://marvel-live.freetls.fastly.net/canvas/2020/2/1974477c97a54aeca692c1df411d8771?quality=95&fake=.png"))
+        childrenImage.add(NetworkImage(path = this.product.imageUrl))
 
         val sizeImage = Size(width = UnitValue(100.0, UnitType.PERCENT), height = UnitValue(250.0, UnitType.REAL))
         var flexImage = Flex(size = sizeImage, flexDirection = FlexDirection.COLUMN, alignItems = Alignment.CENTER,justifyContent = JustifyContent.CENTER)
+        val containerImage = Container(childrenImage)
+        containerImage.applyFlex(flexImage)
+        return containerImage
+    }
 
-        val imageContaier = Container(childrenImage).applyFlex(flexImage)
-        childrenProduct.add(imageContaier)
-
+    fun getInfoContainer(): Container {
         var childrenInfo = mutableListOf<ServerDrivenComponent>()
         childrenInfo.add(Text(product.name))
         childrenInfo.add(Text(product.value))
@@ -44,9 +69,12 @@ class DeatailProductBuilder {
                 alignItems = Alignment.SPACE_BETWEEN,
                 justifyContent = JustifyContent.SPACE_BETWEEN)
 
-        val infoContainer = Container(childrenInfo).applyFlex(flexDetail)
-        childrenProduct.add(infoContainer)
+        val infoContainer = Container(childrenInfo)
+        infoContainer.applyFlex(flexDetail)
+        return  infoContainer
+    }
 
+    fun getDescriptionContainer(): Container {
         var childrenDescription = mutableListOf<ServerDrivenComponent>()
         childrenDescription.add(Text(product.longDescription))
         var flexInfo = Flex(flexWrap = FlexWrap.WRAP,
@@ -54,15 +82,22 @@ class DeatailProductBuilder {
                 alignContent = Alignment.CENTER,
                 alignItems = Alignment.CENTER,
                 justifyContent = JustifyContent.CENTER)
-        val descriptionContainer = Container(childrenDescription).applyFlex(flexInfo)
-        childrenProduct.add(descriptionContainer)
-
-        return  Screen(
-                navigationBar = NavigationBar(title =  product.name, showBackButton = true)
-                ,content = Form(path = "add/${product.sku}",
-                method = FormMethodType.POST,
-                child = Container(childrenProduct)))
+        val descriptionContainer = Container(childrenDescription)
+        descriptionContainer.applyFlex(flexInfo)
+        return  descriptionContainer
     }
+
+    fun getSubmitContainer(): Container {
+        var childrenButton = mutableListOf<ServerDrivenComponent>()
+        val submitButton = Button(text = "Adicionar ao carrinho")
+        val sizeButton = Size(width = UnitValue(90.0, UnitType.PERCENT), height = UnitValue(24.0, UnitType.REAL))
+        var flexButton = Flex(size = sizeButton, flexDirection = FlexDirection.COLUMN, alignItems = Alignment.CENTER,justifyContent = JustifyContent.CENTER)
+        val containerButton = Container(listOf(FormSubmit(submitButton)))
+        containerButton.applyAppearance(Appearance(backgroundColor = "#fa5d6a"))
+        return  containerButton
+    }
+
+
 
 
 }
